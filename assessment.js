082d55a0df2101a083ed5490b9e6fa96d16a732d
Madeleine Bulkow <28768142+mbulkow@users.noreply.github.com>
2018-03-05 10:29:55 -0800

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -7,7 +7,7 @@ After writing your function uncomment the matching function reference at the bot
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+     return 'Hello World!';
 }
 
 /*
@@ -23,8 +23,19 @@ function helloWorld() {
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+     if(num%15 === 0){
+         return 'Lambda School';
+     }
+     else if(num%3 === 0){
+         return 'Lambda';
+     }
+     else if(num%5 === 0){
+         return 'School';
+     }
+     else{
+         return num;     
+     }
 }
 
 /*
@@ -38,8 +49,14 @@ function lambdaSchool() {
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
-
+function longestString(strs) {
+    longestStr = '';
+     strs.forEach(function(element){
+         if(element.length > longestStr.length){
+             longestStr = element;
+         }
+     });
+     return longestStr;
 }
 
 /*
@@ -63,7 +80,12 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
+function computeUserAverageAge(users) {
+    ageSum = 0;
+    users.forEach(function(element){
+        ageSum += element.age;
+    });
+    return Math.round(ageSum/users.length);
 
 }
 
